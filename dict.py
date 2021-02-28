bf559248c90fcede77699c2d0da90025ff392af7
Jay M Gonzalez <jaimungon@gmail.com>
2021-02-28 14:22:45 +0100

    dictionaries finished

--- dict.py
+++ dict.py
@@ -61,27 +61,88 @@
 #     else:
 #         print(f'Please {friend} tell me your fav num')
 
-# Dictionary within dictionary
-users = {
-    'debs': {
-        'name': 'debs',
-        'surname': 'deriu',
-        'age': 31,
-        'city': 'sassari'
-    },
-    'jay': {
-        'name': 'jay',
-        'surname': 'gonzalez',
-        'age': 34,
-        'city': 'sassari'
-    },
-    'gordo': {
-        'name': 'jullien',
-        'surname': 'lloveras',
-        'age': 35,
-        'city': 'buenos aires'
-    },
-}
-for user, details in users.items():
-    full_name = details['name'] + ' ' + details['surname']
-    print(f'{user} full name is: {full_name}')
+# # Dictionary within dictionary
+# users = {
+#     'debs': {
+#         'name': 'debs',
+#         'surname': 'deriu',
+#         'age': 31,
+#         'city': 'sassari'
+#     },
+#     'jay': {
+#         'name': 'jay',
+#         'surname': 'gonzalez',
+#         'age': 34,
+#         'city': 'sassari'
+#     },
+#     'gordo': {
+#         'name': 'jullien',
+#         'surname': 'lloveras',
+#         'age': 35,
+#         'city': 'buenos aires'
+#     },
+# }
+# for user, details in users.items():
+#     full_name = details['name'] + ' ' + details['surname']
+#     print(f'{user} full name is: {full_name}')
+
+# # Dict of animals
+# paquito = {
+#   'type': 'cat',
+#   'owner': 'jay'
+# },
+# eva = {
+#   'type': 'dog',
+#   'owner': 'ivan'
+# },
+# biaggo = {
+#   'type': 'cat',
+#   'owner': 'marine'
+# },
+# ettore = {
+#   'type': 'dog',
+#   'owner': 'marine'
+# },
+# pets = [paquito, eva, biaggo, ettore]
+
+# for pet in pets:
+#     print(pet)
+
+# # Fav places
+# fav_places = {
+#     'jay': [
+#         'cali',
+#         'mazunte',
+#         'pai'
+#     ],
+#     'debs': [
+#         'brighton',
+#         'sassari'
+#     ],
+#     'paco': [
+#         'robledo'
+#     ]
+# }
+# for name, places in fav_places.items():
+#     print(f'{name} favourite places are {places}')
+
+# # Cities
+# cities = {
+#     'madrid': {
+#         'population': 4500000,
+#         'language': 'spanish',
+#         'country': 'spain'
+#     },
+#     'sassari': {
+#         'population': 150000,
+#         'language': 'italian',
+#         'country': 'italy'
+#     },
+#     'brighton': {
+#         'population': 250000,
+#         'language': 'english',
+#         'country': 'uk'
+#     }
+# }
+# for name, data in cities.items():
+#     print(f'{name.title()} has {data["population"]} inhabitants, they speak {data["language"]} and the country it\'s {data["country"]}')
